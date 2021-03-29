cc657352ac8af5e45cb667f5262bcd22aaf94560
Edson Batista <edson.batista@catho.com>
2021-03-29 20:40:09 -0300

    refactor: create Content Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { useEffect, useState } from 'react';
 
-import { MovieCard } from './components/MovieCard';
-
 import { SideBar } from './components/SideBar';
 // import { Content } from './components/Content';
 
@@ -11,6 +9,7 @@ import './styles/global.scss';
 
 import './styles/sidebar.scss';
 import './styles/content.scss';
+import { Content } from './components/Content';
 
 interface GenreResponseProps {
   id: number;
@@ -53,13 +52,7 @@ export function App() {
           <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
         </header>
 
-        <main>
-          <div className="movies-list">
-            {movies.map(movie => (
-              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-            ))}
-          </div>
-        </main>
+        <Content movies={movies} />
       </div>
     </div>
   )
